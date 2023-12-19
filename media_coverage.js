6531d654e805b0de6c69c6498892f2e92c0e631d
Ok945 <omkendre5454@gmail.com>
2023-12-19 13:35:31 +0530

    some changes

---
+++ media_coverage.js
@@ -0,0 +1,105 @@
+const popular = document.querySelector(".popular");
+document.querySelectorAll(".popular").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+
+const popular2 = document.querySelector(".popular2");
+document.querySelectorAll(".popular2").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular2").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular3 = document.querySelector(".popular3");
+document.querySelectorAll(".popular3").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular3").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular4 = document.querySelector(".popular4");
+document.querySelectorAll(".popular4").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular4").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular5 = document.querySelector(".popular5");
+document.querySelectorAll(".popular5").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular5").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular6 = document.querySelector(".popular6");
+document.querySelectorAll(".popular6").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular6").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular7 = document.querySelector(".popular7");
+document.querySelectorAll(".popular7").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular7").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular8 = document.querySelector(".popular8");
+document.querySelectorAll(".popular8").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular8").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular9 = document.querySelector(".popular9");
+document.querySelectorAll(".popular9").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular9").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+const popular10 = document.querySelector(".popular10");
+document.querySelectorAll(".popular10").forEach((item) => {
+  item.classList.remove("opened");
+  });
+document.querySelectorAll(".popular10").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+
+
+
+
