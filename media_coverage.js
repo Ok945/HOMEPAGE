92716c68ad9779f416f53562d041bdd07fdbfc70
Ok945 <omkendre5454@gmail.com>
2023-12-19 16:50:14 +0530

    some changes

--- media_coverage.js
+++ media_coverage.js
@@ -1,18 +1,19 @@
 const popular = document.querySelector(".popular");
 document.querySelectorAll(".popular").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
-  });
+
+  })
 });
 
 
 const popular2 = document.querySelector(".popular2");
 document.querySelectorAll(".popular2").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular2").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -22,7 +23,7 @@ document.querySelectorAll(".popular2").forEach((item) => {
 const popular3 = document.querySelector(".popular3");
 document.querySelectorAll(".popular3").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular3").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -32,7 +33,7 @@ document.querySelectorAll(".popular3").forEach((item) => {
 const popular4 = document.querySelector(".popular4");
 document.querySelectorAll(".popular4").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular4").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -42,7 +43,7 @@ document.querySelectorAll(".popular4").forEach((item) => {
 const popular5 = document.querySelector(".popular5");
 document.querySelectorAll(".popular5").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular5").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -52,7 +53,7 @@ document.querySelectorAll(".popular5").forEach((item) => {
 const popular6 = document.querySelector(".popular6");
 document.querySelectorAll(".popular6").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular6").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -62,7 +63,7 @@ document.querySelectorAll(".popular6").forEach((item) => {
 const popular7 = document.querySelector(".popular7");
 document.querySelectorAll(".popular7").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular7").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -72,7 +73,7 @@ document.querySelectorAll(".popular7").forEach((item) => {
 const popular8 = document.querySelector(".popular8");
 document.querySelectorAll(".popular8").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular8").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -82,7 +83,7 @@ document.querySelectorAll(".popular8").forEach((item) => {
 const popular9 = document.querySelector(".popular9");
 document.querySelectorAll(".popular9").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular9").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
@@ -92,7 +93,7 @@ document.querySelectorAll(".popular9").forEach((item) => {
 const popular10 = document.querySelector(".popular10");
 document.querySelectorAll(".popular10").forEach((item) => {
   item.classList.remove("opened");
-  });
+});
 document.querySelectorAll(".popular10").forEach((item) => {
   item.addEventListener("click", (event) => {
     item.classList.toggle("opened");
